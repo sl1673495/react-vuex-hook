870ada89b77280d652c519b6a7884a3eaa1d1240
ssh <949589855@qq.com>
2019-11-03 15:18:38 +0800

    feat: 项目更名为reax-hook

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ const { resolve } = path
 module.exports = {
   mode: 'production',
   entry: {
-    app: resolve('./src/index.tsx'),
+    app: resolve('./src/index.jsx'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)/,
+        include: [resolve('src')],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          "presets": ["@babel/preset-env", "@babel/preset-react"],
+        }
+      },
+    ],
   },
   externals: {
     react: 'React'
