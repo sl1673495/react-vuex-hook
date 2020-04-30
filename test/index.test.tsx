b64a96104982b042bc5197b7ab8e0f6c78f2b6a8
ssh <949589855@qq.com>
2020-04-30 15:01:42 +0800

    refactor: changes test case descriptions to English

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { cleanup, render, fireEvent } from '@testing-library/react';
-import { renderHook, act } from '@testing-library/react-hooks';
-import initStore from '../src';
+import React from "react"
+import { cleanup, render, fireEvent } from "@testing-library/react"
+import { renderHook, act } from "@testing-library/react-hooks"
+import initStore from "../src"
 
-const wait = (time: number) => new Promise(resolve => setTimeout(resolve, time));
+const wait = (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time))
 
 const { connect, useStore } = initStore({
   getInitState: () => ({
@@ -12,30 +13,30 @@ const { connect, useStore } = initStore({
   mutations: {
     // 浅拷贝state
     add(state, payload) {
-      return Object.assign({}, state, { count: state.count + 1 });
+      return Object.assign({}, state, { count: state.count + 1 })
     },
   },
   getters: {
     countPlusOne(state) {
-      return state.count + 1;
+      return state.count + 1
     },
   },
   actions: {
     async asyncAdd({ dispatch, state, getters }, payload) {
-      await wait(0);
-      dispatch({ type: 'add' });
+      await wait(0)
+      dispatch({ type: "add" })
       // 返回的值会被包裹的promise resolve
-      return true;
+      return true
     },
   },
-});
+})
 
 function Comp({ children }) {
-  const { state, getters, dispatch } = useStore();
-  const onAdd = () => dispatch({ type: 'add' });
-  const onErrorType = () => dispatch({ type: 'ERROR_TYPE' } as any);
+  const { state, getters, dispatch } = useStore()
+  const onAdd = () => dispatch({ type: "add" })
+  const onErrorType = () => dispatch({ type: "ERROR_TYPE" } as any)
   // 异步的add
-  const asyncAdd = () => dispatch.action({ type: 'asyncAdd' });
+  const asyncAdd = () => dispatch.action({ type: "asyncAdd" })
   return (
     <div>
       <span data-testid="count">{state.count}</span>
@@ -45,83 +46,88 @@ function Comp({ children }) {
       <button data-testid="error" onClick={onErrorType}></button>
       {children}
     </div>
-  );
+  )
 }
 
-afterEach(cleanup);
+afterEach(cleanup)
 // 调用connect包裹需要使用useStore的组件
-const Connected = connect(Comp);
+const Connected = connect(Comp)
 
-describe('正常使用', () => {
-  test('state和mutations表现正常', () => {
-    const { getByTestId } = render(<Connected />);
-    expect(getByTestId('count').innerHTML).toBe('1');
-    fireEvent.click(getByTestId('add'));
-    expect(getByTestId('count').innerHTML).toBe('2');
-  });
+describe("Connect to a component", () => {
+  test("should state changed by mutation", () => {
+    const { getByTestId } = render(<Connected />)
+    expect(getByTestId("count").innerHTML).toBe("1")
+    fireEvent.click(getByTestId("add"))
+    expect(getByTestId("count").innerHTML).toBe("2")
+  })
 
-  test('getters表现正常', () => {
-    const { getByTestId } = render(<Connected />);
-    expect(getByTestId('countPlusOne').innerHTML).toBe('2');
-    fireEvent.click(getByTestId('add'));
-    expect(getByTestId('countPlusOne').innerHTML).toBe('3');
-  });
+  test("should getter changed by mutation", () => {
+    const { getByTestId } = render(<Connected />)
+    expect(getByTestId("countPlusOne").innerHTML).toBe("2")
+    fireEvent.click(getByTestId("add"))
+    expect(getByTestId("countPlusOne").innerHTML).toBe("3")
+  })
 
-  test('异步actions派发后，state和getters表现正常', async () => {
-    const { result } = renderHook(() => useStore(), { wrapper: Connected });
-    const { dispatch } = result.current;
+  test("should state and getter changed by async action", async () => {
+    const { result } = renderHook(() => useStore(), { wrapper: Connected })
+    const { dispatch } = result.current
 
     await act(async () => {
       await dispatch.action({
-        type: 'asyncAdd',
-      });
+        type: "asyncAdd",
+      })
 
-      const { state, getters } = result.current;
-      expect(state.count).toBe(2);
-      expect(getters.countPlusOne).toBe(3);
-    });
-  });
+      const { state, getters } = result.current
+      expect(state.count).toBe(2)
+      expect(getters.countPlusOne).toBe(3)
+    })
+  })
 
-  test('异步actions派发前后，对应的loading状态表现正常，并且测试any的两种参数格式', async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useStore(), { wrapper: Connected });
-    const { dispatch } = result.current;
-    expect(result.current.state.loadingMap.any(['asyncAdd'])).toBeFalsy();
+  test("should correctly change loading state before and after async action", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useStore(), {
+      wrapper: Connected,
+    })
+    const { dispatch } = result.current
+    expect(result.current.state.loadingMap.any(["asyncAdd"])).toBeFalsy()
     await act(async () => {
       dispatch.action({
-        type: 'asyncAdd',
-      });
-      await waitForNextUpdate();
-      expect(result.current.state.loadingMap.any('asyncAdd')).toBeTruthy();
-      await waitForNextUpdate();
-      expect(result.current.state.loadingMap.any(['asyncAdd'])).toBeFalsy();
-    });
-  });
+        type: "asyncAdd",
+      })
+      await waitForNextUpdate()
+      expect(result.current.state.loadingMap.any("asyncAdd")).toBeTruthy()
+      await waitForNextUpdate()
+      expect(result.current.state.loadingMap.any(["asyncAdd"])).toBeFalsy()
+    })
+  })
 
-  test('dispatch action时如果type传错会抛错', async () => {
-    const { result } = renderHook(() => useStore(), { wrapper: Connected });
-    const { dispatch } = result.current;
+  test("should throw an exception when errorly", async () => {
+    const { result } = renderHook(() => useStore(), { wrapper: Connected })
+    const { dispatch } = result.current
 
-    const throwFn = async () => await dispatch.action({ type: 'ERROR_TYPE' } as any);
+    const throwFn = async () =>
+      await dispatch.action({ type: "ERROR_TYPE" } as any)
     await act(async () => {
       try {
-        await throwFn();
+        await throwFn()
       } catch (e) {
-        expect(e).toBeInstanceOf(Error);
+        expect(e).toBeInstanceOf(Error)
       }
-    });
-  });
+    })
+  })
 
-  test('不传参的情况下调用initStore渲染正常', () => {
-    const { connect } = initStore();
+  test("should work with empty args", () => {
+    const { connect } = initStore()
     const EmptyConnect = connect(() => {
-      return <span data-testid="empty">empty</span>;
-    });
-    const { getByTestId } = render(<EmptyConnect />);
-    expect(getByTestId('empty').innerHTML).toBe('empty');
-  });
+      return <span data-testid="empty">empty</span>
+    })
+    const { getByTestId } = render(<EmptyConnect />)
+    expect(getByTestId("empty").innerHTML).toBe("empty")
+  })
 
-  test('无意义的调用loadingMap.any不会报错', async () => {
-    const { result } = renderHook(() => useStore(), { wrapper: Connected });
-    expect(result.current.state.loadingMap.any(['ERROR_TYPE'] as any)).toBe(false);
-  });
-});
+  test("should not crash when calling loadingMap.any for a non-existent action type", async () => {
+    const { result } = renderHook(() => useStore(), { wrapper: Connected })
+    expect(result.current.state.loadingMap.any(["ERROR_TYPE"] as any)).toBe(
+      false,
+    )
+  })
+})
